eb9e8fca4b11b5177e3f43ea49ab425d8c390221
Twitty <btwitty187@gmail.com>
2020-05-21 21:54:46 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {TabsPage} from './tabs/tabs.page';
 
 const routes: Routes = [
   {
